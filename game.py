33c089853c85de5a6fc52a71fa25921e6ab4af66
VovLer G <126910533+DaregonPL@users.noreply.github.com>
2024-03-15 19:11:55 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -2,23 +2,46 @@ import time
 
 
 class TheLife():
-    def __init__(self, resolution):
-        self.h, self.w = [int(x) for x in resolution.split('x')]
-        self.map = Map(self.h, self.w)
+    def __init__(self):
+        self.height, self.width = 25, 25
 
-    def print(self):
-        [print(' '.join(x)) for x in self.map.field]
+    def setup(self, **kw):
+        for n, v in kw.items():
+            if n == 'height':
+                self.height = v
+            elif n == 'width':
+                self.width = v
+
+    def run(self):
+        self.map = Map(self.height, self.width)
+        while True:
+            print(self.map.frames)
+            print('\n'.join([' '.join(x) for x in
+                             self.apply_cord(self.map.lastframe())]))
+            self.map.mark(*input().split(';'))
+
+    def apply_cord(self, matrix):
+        mtx = matrix.copy()
+        return [[' '] + [str(x) for x in range(self.width)]] + \
+               [[str(x)] + mtx[x] for x in range(self.height)]
 
 
 class Map():
     def __init__(self, height, width):
         self.h, self.w = height, width
-        self.field = [['*' for x in range(width)] for y in range(height)]
+        self.field = [['□' for x in range(width)] for y in range(height)]
+        self.frames = []
 
-    def run(self):
-        self.next = self.field.copy()
-        
+    def generate(self):
+        self.frames.append([x for x in self.field])
+
+    def mark(self, x, y):
+        self.field[int(x)][int(y)] = '■'
+
+    def lastframe(self):
+        return [x for x in self.field]
 
 
-life = TheLife('10x30')
-life.print()
+life = TheLife()
+life.setup(height=2, width=5)
+life.run()
